f14dc57c4b0fa04a6d8db8352ab276dadf0a7955
Michele Minchillo <michele.minchillo@bdo.it>
2018-01-25 18:10:27 +0100

    grafico home layout

--- APT_ArchV03/Controllers/HomeController.cs
+++ APT_ArchV03/Controllers/HomeController.cs
@@ -3,13 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using APT_ArchV03.Models;
 
 namespace APT_ArchV03.Controllers
 {
     public class HomeController : Controller
     {
+        private Db_APT_ArchEntities db = new Db_APT_ArchEntities(); //da all'accesso al database
+
         public ActionResult Index()
         {
+            
+            List<Caw> caws = new List<Caw>(); //creo lista caws
+            caws = (from c in db.Caws select c).ToList(); //tramite linq creo una collection che converto in lista e assegno a variabile caws
+            
+            float opened = caws.Count(x => x.caw_status == 1); //metto in una variabile di tipo float tutti i caws di stato 1
+            float reporting = caws.Count(x => x.caw_status == 2); //metto in una variabile di tipo float tutti i caws di stato 2
+            float closed = caws.Count(x => x.caw_status == 3); //metto in una variabile di tipo float tutti i caws di stato 3
+            float total = opened + reporting + closed;
+
+            ViewData["opened"] = opened;
+            ViewData["reporting"] = reporting;
+            ViewData["closed"] = closed;
+            ViewData["total"] = total;
+
+            opened = (opened / total * 100);
+            reporting = reporting / total * 100;
+            closed = closed / total * 100;
+
+         
+            System.Globalization.CultureInfo culture = new System.Globalization.CultureInfo("en-US");
+            ViewData["openedperc"] = Convert.ToString(Math.Round(opened,0), culture); //variabile opened passata alla vista tramite ViewData
+            ViewData["reportingperc"] = Convert.ToString(Math.Round(reporting, 0), culture);
+            ViewData["closedperc"] = Convert.ToString(Math.Round(closed, 0), culture);
+
             return View();
         }
 
@@ -26,5 +53,7 @@ namespace APT_ArchV03.Controllers
 
             return View();
         }
+
+
     }
 }
